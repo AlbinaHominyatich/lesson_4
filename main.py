7308d61264184928a5ef2c84647e5d99622028d2
AlbinaHominyatich <hominyatichalbina@gmail.com>
2023-04-27 18:39:36 +0300

    args

---
+++ main.py
@@ -0,0 +1,10 @@
+#теорія
+print("lkdfwek", 2123, 9.0)
+
+def black_hole(*args):
+    print(type(args))
+    print(args)
+    for argument in args:
+        print(argument)
+
+black_hole(234, "Earth", "rusnya", "time", 6776)
