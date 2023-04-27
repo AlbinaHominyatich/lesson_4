d6e7f67198295b477686e4ba09cf58ae28299e39
AlbinaHominyatich <hominyatichalbina@gmail.com>
2023-04-27 18:47:57 +0300

    kwargs

--- main.py
+++ main.py
@@ -8,3 +8,10 @@ def black_hole(*args):
         print(argument)
 
 black_hole(234, "Earth", "rusnya", "time", 6776)
+
+def black_hole_named(**kwargs):
+    print(type(kwargs))
+    print(kwargs)
+    for argument in kwargs:
+        print(argument)
+black_hole_named(name="Gleb", planet="Earth", number = 5)
